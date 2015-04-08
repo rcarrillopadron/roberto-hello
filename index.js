a15b61df210e829617f4e6f8ea64a2c495fbedda
Roberto Carrillo <email@robertocarrillo.com>
2015-04-07 20:58:20 -0500

    Now with ExpressJS

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello Roberto");
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+   res.send("Hello Roberto");
+});
+
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Listening...");
+});
